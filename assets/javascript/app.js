c4f8f77241516544ab11e78929084152a03b81ac
Nommington <rymckenz@gmail.com>
2018-04-19 15:17:51 -0400

    added some logic, populated placeholder images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,51 +5,77 @@ var spentHumans = [];
 var spentCylons = [];
 var timeLeft=5;
 
-    $("body").on("click", "#startBtn", function() {
-        timeLeft = 5;
-        // Generates a group of 3 humans and 1 cylon into imgTray
-        while (spentCylons.length<12) {
-            var imgTray = [];
-            k=0;
-            while (k<3) {
-                var humanVar = humans[Math.floor(Math.random()*humans.length)];
-                //console.log(humanVar);
-                if (spentHumans.indexOf(humanVar)===-1) {
-                    spentHumans.push(humanVar);
-                    imgTray.push(humanVar);
-                    k++;
-                }
-            }
-            cylonPop = true;
-            while (cylonPop) {
-                var cylonVar = cylons[Math.floor(Math.random()*cylons.length)];
-                if (spentCylons.indexOf(cylonVar)===-1) {
-                    spentCylons.push(cylonVar);
-                    imgTray.push(cylonVar);
-                    cylonPop = false;
-                }
-            }
-            //console.log(imgTray);
-            //next we randomize imgTray
-            var cylonShuffle = [];
-            j=0;
-            while (j<4) {
-                var shuffledOption = imgTray[Math.floor(Math.random()*4)];
-                if (cylonShuffle.indexOf(shuffledOption)===-1) {
-                    cylonShuffle.push(shuffledOption);
-                    j++;
-                }
-            }
-            console.log(cylonShuffle);
-            //now we populate the four clickable images
-            for (l=0; l<4; l++) {
-                var colonistIcon = $("<img>");
-                colonistIcon.addClass("possibleToaster");
-                colonistIcon.attr("src", "assets/images/"+cylonShuffle[l]);
-                colonistIcon.attr("alt", cylonShuffle[l]);
-                colonistIcon.attr("id", cylonShuffle[l]);
-                $("#gameArea").append(colonistIcon);
-            }
+    $("body").on("click", "#startBtn", function choose() {
+        $("#gameArea").empty();
+        if (spentCylons.length===12) {
+            //display stats
+            console.log("DONE!");
         }
+        else {
+            timeLeft = 5;
+            // Generates a group of 3 humans and 1 cylon into imgTray
+            // while (spentCylons.length<12) {
+                var imgTray = [];
+                k=0;
+                while (k<3) {
+                    var humanVar = humans[Math.floor(Math.random()*humans.length)];
+                    //console.log(humanVar);
+                    if (spentHumans.indexOf(humanVar)===-1) {
+                        spentHumans.push(humanVar);
+                        imgTray.push(humanVar);
+                        k++;
+                    }
+                }
+                cylonPop = true;
+                while (cylonPop) {
+                    var cylonVar = cylons[Math.floor(Math.random()*cylons.length)];
+                    if (spentCylons.indexOf(cylonVar)===-1) {
+                        spentCylons.push(cylonVar);
+                        imgTray.push(cylonVar);
+                        cylonPop = false;
+                    }
+                }
+                //console.log(imgTray);
+                //next we randomize imgTray
+                var cylonShuffle = [];
+                j=0;
+                while (j<4) {
+                    var shuffledOption = imgTray[Math.floor(Math.random()*4)];
+                    if (cylonShuffle.indexOf(shuffledOption)===-1) {
+                        cylonShuffle.push(shuffledOption);
+                        j++;
+                    }
+                }
+                console.log(cylonShuffle);
+                //now we populate the four clickable images
+                for (l=0; l<4; l++) {
+                    var colonistIcon = $("<img>");
+                    colonistIcon.addClass("possibleToaster");
+                    colonistIcon.attr("src", "assets/images/"+cylonShuffle[l]);
+                    colonistIcon.attr("alt", cylonShuffle[l]);
+                    colonistIcon.attr("id", cylonShuffle[l]);
+                    $("#gameArea").append(colonistIcon);
+                }
+                cylonDetector = false;
+                $("#gameArea").on("click", ".possibleToaster", function(){
+                    var shuffleId = $(this).attr("id");
+                    console.log(shuffleId);
+                    var imgPosition = (imgTray).indexOf(shuffleId);
+                    console.log(imgPosition);
+                    if (imgPosition===3) {
+                        cylonDetector = true;
+                        correct();
+                        console.log("CYLON!");
+                    }
+                });
+            }
     });
+    function incorrect() {
+
+    }
+    function correct() {
+        //add a timer here
+        setTimeout(choose, 3000);
+        $("#gameArea").append("<p>CYLON IDENTIFIED.</p><p>Well done, cadet.</p>");
+    }
 });
